36dd6a5796c76e90ae473bc5d520c8f1e548ed59
Rohman <bluekraken9999@gmail.com>
2025-12-11 11:23:59 +0800

    chore: Edit

--- app/pages/AboutPage/page.tsx
+++ app/pages/AboutPage/page.tsx
@@ -28,7 +28,7 @@ export default function AboutPage() {
 
   return (
     <div
-      className="min-h-screen flex items-center justify-center p-0 md:pl-32 pb-24 md:pb-6"
+      className="min-h-screen flex items-center justify-center p-0 md:pl-32 pb-24 md:pb-6 p-8"
       style={{ background: getColor("background") }}
     >
       <div className="max-w-4xl w-full pb-18">
